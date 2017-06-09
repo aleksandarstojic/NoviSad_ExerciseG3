5f33b0adda975ae62d5c5fc4b23483221adef51b
somename <someemail>
2017-06-09 14:59:23 +0200

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -20,7 +20,7 @@ public class Frame {
 
 	//returns the score of a single frame
 	public int score(){
-		int score = getFirstThrow + this.secondThrow;
+		int score = getFirstThrow + getSecondThrow();
 		
 		return score;
 	}
