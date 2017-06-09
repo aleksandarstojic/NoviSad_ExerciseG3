bf3143245b47532d683a324cbecd8ff958670693
somename <someemail>
2017-06-09 15:27:15 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -80,6 +80,8 @@ public class TestBowling {
 		addFrames(bowlingGame, frame, 9);
 		Frame frame1 = new Frame(6,4);
 		bowlingGame.addFrame(frame1);
+		boolean check = bowlingGame.getFrame(9).isSpare();
+		assertTrue(check);
 		
 	}
 	/// BOWLING GAME TESTS ///
