404a063d49b7a56fdc02d0ea85521d0b208e3cff
somename <someemail>
2017-06-09 14:44:24 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -24,4 +24,10 @@ public class TestBowling {
 	
 	/// HELPER METHODS ///
 	public void addFrames(BowlingGame bowlingGame, Frame frame)
+	{
+		for (int i=0; i<15; i++)
+		{
+			bowlingGame.addFrame(frame);
+		}
+	}
 }
