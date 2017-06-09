6129e8688f329d2259fb62016c9bc671d6f7824c
somename <someemail>
2017-06-09 15:17:38 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -55,7 +55,7 @@ public class TestBowling {
 		addFrames(bowlingGame, frame, 8);
 		Frame frame1 = new Frame (10,0);
 		bowlingGame.addFrame(frame1);
-		int actual = bowlingGame.getFrame(8);
+		int actual = bowlingGame.getFramesScore(8);
 		int expected = 10; // because I want the last frame's score and it should be 10 because I've instantiated it with score 10
 		assertEquals(actual, expected);
 	}
