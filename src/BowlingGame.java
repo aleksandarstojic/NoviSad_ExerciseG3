07d3468a1fb5460e02c264d1ea91da38b85ac3c3
somename <someemail>
2017-06-09 15:32:20 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -34,7 +34,8 @@ public class BowlingGame {
 	
 	// Sets the bonus throws at the end of the game
 	public void setBonus(int firstThrow, int secondThrow) {
-		if (frames.get(frames.size()-1)
+		if (frames.get(frames.size()-1).isSpare() || frames.get(frames.size()-1).isStrike())
+			bonus = 0;
 	}
 	
 	// Returns the game score
