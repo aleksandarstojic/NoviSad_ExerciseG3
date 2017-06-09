0c126d2ef60e0737d633202eb050783f11ee7d91
somename <someemail>
2017-06-09 14:46:32 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -7,19 +7,16 @@ public class TestBowling {
 	@Test(expected=BowlingException.class)
 	public void AddingTheFrameTest() {
 		BowlingGame bowlingGame = new BowlingGame();
-		Frame frame = new Frame();
-		bowlingGame.addFrame(frame);
-		
-		
+		Frame frame = new Frame();	
 		addFrames(bowlingGame, frame);
 		
 	}
 	
 	
 	/// HELPER METHODS ///
-	public void addFrames(BowlingGame bowlingGame, Frame frame)
+	public void addFrames(BowlingGame bowlingGame, Frame frame, int count)
 	{
-		for (int i=0; i<15; i++)
+		for (int i=0; i<count; i++)
 		{
 			bowlingGame.addFrame(frame);
 		}
